7dceb3881e3f7e9a3d445a89d0819300e01de7c8
Christian Fobel <christian@fobel.net>
2016-12-19 05:58:17 -0500

    feat(address-of): add dump_address_of_header func

--- cpp_delegate/__init__.py
+++ cpp_delegate/__init__.py
@@ -4,6 +4,8 @@ import json
 import clang_helpers.clang_ast as ca
 import path_helpers as ph
 
+from .address_of import get_attributes, render
+
 
 def dump_cpp_ast(env):
     project_dir = ph.path(env['PROJECT_DIR'])
@@ -16,6 +18,21 @@ def dump_cpp_ast(env):
 
     with lib_dir.joinpath('cpp_ast.json').open('w') as output:
         json.dump(cpp_ast_json, output, indent=2)
+    return cpp_ast_json
+
+
+def dump_address_of_header(env, cpp_ast_json):
+    project_dir = ph.path(env['PROJECT_DIR'])
+    project_name = project_dir.name.replace('-', '__')
+    lib_dir = project_dir.joinpath('lib', project_name)
+    lib_dir.makedirs_p()
+
+    attributes = get_attributes(cpp_ast_json['members'])
+    header_content = render(cpp_ast_json, attributes)
+
+    with lib_dir.joinpath('AddressOf.h').open('w') as output:
+        output.write(header_content)
+    return header_content
 
 
 def parse_cpp_ast(source, env):
