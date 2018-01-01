4b94fd357172aaaa8e67686418ef6390d1a031ea
Christian Fobel <christian@fobel.net>
2018-01-01 16:53:04 -0500

    fix(address-of): exclude toolchain attributes

--- cpp_delegate/__init__.py
+++ cpp_delegate/__init__.py
@@ -21,6 +21,26 @@ def dump_cpp_ast(env):
     return cpp_ast_json
 
 
+def _isindir(root, file_path):
+    '''
+    Parameters
+    ----------
+    root : str
+        Root directory.
+    file_path : str
+        File to test for membership in root.
+
+    Returns
+    -------
+    bool
+        ``True`` if file is contained within directory structure under
+        specified root.
+    '''
+    root = ph.path(root).realpath()
+    file_path = ph.path(file_path).realpath()
+    return not root.relpathto(file_path).startswith('..')
+
+
 def dump_address_of_header(env, cpp_ast_json):
     project_dir = ph.path(env['PROJECT_DIR'])
     project_name = project_dir.name.replace('-', '__')
@@ -33,6 +53,12 @@ def dump_address_of_header(env, cpp_ast_json):
                    output_path=output_path))
 
     attributes = get_attributes(cpp_ast_json['members'])
+    # Path to ARM toolchain.
+    toolchain_dir = ph.path(env['PIOHOME_DIR']).joinpath('packages',
+                                                         'toolchain-'
+                                                         'gccarmnoneeabi')
+    attributes = {k: v for k, v in attributes.iteritems()
+                  if not _isindir(toolchain_dir, v['location']['file'])}
     header_content = render(cpp_ast_json, attributes)
 
     with output_path.open('w') as output:
